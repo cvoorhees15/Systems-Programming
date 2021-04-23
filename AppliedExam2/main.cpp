81cdca4d56399382fd362cf3e6d868c149889cf1
Caleb Voorhees <c.voorhees@setonhill.edu>
2021-04-23 12:27:34 -0400

    Initial Commit

---
+++ AppliedExam2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AppliedExam2
+//
+//  Created by Caleb Voorhees on 4/23/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
